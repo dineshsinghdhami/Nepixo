d97f890023eee74f8cbc0ae1f53fda1dd498b833
Dinesh Singh Dhami <dineshdhamidn@gmail.com>
2026-07-23 21:58:16 +0545

    Update settings.py

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -140,7 +140,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587  
 EMAIL_USE_TLS = True  
 EMAIL_USE_SSL = False  
-EMAIL_HOST_USER = 'info.nepixo@gmail.com'  
-EMAIL_HOST_PASSWORD = 'lpsz rlla vynn kxhd'  
+EMAIL_HOST_USER = '#######@gmail.com'  
+EMAIL_HOST_PASSWORD = '###########'  
 
-DEFAULT_FROM_EMAIL = 'info.nepixo@gmail.com'
+DEFAULT_FROM_EMAIL = '#######@gmail.com'
